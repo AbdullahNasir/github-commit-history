c4337404982b346b1156399b7bd4330668fb4d3d
Abdullah Nasir <abdullah555@yahoo.com>
2019-12-23 17:49:21 +0500

    added a method to fetch branches

--- src/app/github-api-service.service.ts
+++ src/app/github-api-service.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { GithubBranch } from './interfaces/github-branch';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class GithubApiServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  fetchBranches(respositoryUrl: string): Promise<GithubBranch[]> {
+    return this.http.get<GithubBranch[]>(`${respositoryUrl}/branches`).toPromise();
+  }
+
 
 }
