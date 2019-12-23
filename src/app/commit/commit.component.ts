56ce3bc7c7469f2e5238f886e803013a40b80743
Abdullah Nasir <abdullah555@yahoo.com>
2019-12-23 21:46:16 +0500

    implemented the functionality of commit component

--- src/app/commit/commit.component.ts
+++ src/app/commit/commit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { GithubCommit } from '../interfaces/github-commit';
 
 @Component({
   selector: 'app-commit',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommitComponent implements OnInit {
 
+  @Input() commit: GithubCommit;
+
   constructor() { }
 
   ngOnInit() {
+    this.commit.commit.message = this.commit.commit.message.split('\n')[0];
+
   }
 
 }
