84ef969cf7aaa7421edf8418d40ba01f6c8506d8
Abdullah Nasir <abdullah555@yahoo.com>
2019-12-23 21:44:50 +0500

    created methods to fetch commits and formatting the data

--- src/app/github-api-service.service.ts
+++ src/app/github-api-service.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { GithubBranch } from './interfaces/github-branch';
 import { HttpClient } from '@angular/common/http';
+import { GithubCommit } from './interfaces/github-commit';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class GithubApiServiceService {
@@ -11,5 +13,32 @@ export class GithubApiServiceService {
     return this.http.get<GithubBranch[]>(`${respositoryUrl}/branches`).toPromise();
   }
 
+  fetchCommits(respositoryUrl: string, branchName: string): Promise<GithubCommit[]> {
+    return this.http.get(`${respositoryUrl}/commits?sha=${branchName}`)
+      .map((commits: Object[]): GithubCommit[] => {
+        return commits.map((commit) => {
+          return {
+            sha: commit['sha'],
+            commit: {
+              author: commit['commit']['author'],
+              message: commit['commit']['message']
+            },
+            url: this.createGithubCommitUrl(respositoryUrl, commit['sha']),
+            date: commit['commit']['author']['date'],
+            historyUrl: this.createGithubHistoryUrl(respositoryUrl, commit['sha'])
+          };
+        });
+      }).toPromise();
+  }
+
+  private createGithubCommitUrl(respositoryUrl: string, sha: string) {
+    respositoryUrl = respositoryUrl.replace('api.', '').replace('repos/', '');
+    return `${respositoryUrl}/commit/${sha}`;
+  }
+
+  private createGithubHistoryUrl(respositoryUrl: string, sha: string) {
+    respositoryUrl = respositoryUrl.replace('api.', '').replace('repos/', '');
+    return `${respositoryUrl}/tree/${sha}`;
+  }
 
 }
