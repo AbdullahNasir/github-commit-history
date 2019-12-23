abc804f9d0f4078aaa57b2190d3517a5226b72f3
Abdullah Nasir <abdullah555@yahoo.com>
2019-12-23 21:45:19 +0500

    implementd the functionality for commits list component

--- src/app/commits-list/commits-list.component.ts
+++ src/app/commits-list/commits-list.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { GithubCommit } from '../interfaces/github-commit';
+import { GithubApiServiceService } from '../github-api-service.service';
+import * as _ from 'lodash';
+import { GithubCommitsGrouped } from '../interfaces/github-commit-grouped';
 
 @Component({
   selector: 'app-commits-list',
   templateUrl: './commits-list.component.html',
   styleUrls: ['./commits-list.component.css']
 })
-export class CommitsListComponent implements OnInit {
+export class CommitsListComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  keys = Object.keys;
+
+  @Input() repositoryUrl: string;
+  @Input() branch: string;
+  groupedCommits: GithubCommitsGrouped = {};
+
+  constructor(private githubApi: GithubApiServiceService) { }
 
   ngOnInit() {
   }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.branch.currentValue) {
+      this.fetchCommits();
+    }
+  }
+
+  async fetchCommits() {
+    const commits: GithubCommit[] = await this.githubApi.fetchCommits(this.repositoryUrl, this.branch);
+    this.groupedCommits = _.groupBy(commits, commit => commit.date.split('T')[0]);
+  }
+
+
 
 }
